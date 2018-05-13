896ba919685a75a86d1cb01bad45353b6a096b5a
celostad <celostad@gmail.com>
2018-05-13 16:15:08 -0300

    Consumindo primeiros dados -aula32

---
+++ src/providers/moovie/moovie.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+/*
+  Generated class for the MoovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MoovieProvider {
+  private baseApiPath ="https://api.themoviedb.org/3";
+
+  constructor(public http: Http) {
+    console.log('Hello MoovieProvider Provider');
+  }
+
+  getLatestMoovies(){
+    return this.http.get(this.baseApiPath + "/movie/popular?api_key=" + this.getApiKey());
+  }
+
+  getApiKey():string {
+    return "aa1a0d5462e56d8e4e19c559d33ea972";
+  }
+}
